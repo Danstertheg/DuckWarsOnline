59256df57a480b59832d5efbfdb1e04285d850cc
Danny Silvestre <dsylvester2017@fau.edu>
2021-12-17 11:01:38 -0500

    stylesheet

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ io.on('connection', (socket) => {
     socket.on('chatmessage', (msg) => {
       io.emit('chatmessage', msg);
     });
+
+    socket.on('playerJoined', (playerInitObj) => {
+        console.log("type of playerInitObj:  " + typeof(playerInitObj))
+        console.log(playerInitObj)
+        console.log(playerInitObj['id'])
+        console.log(playerInitObj['color'])
+        io.emit('playerJoined', playerInitObj);
+      });
+      socket.on('playerMovement',(playerMov) => {
+        io.emit('playerMovement', playerMov);
+      })
   });
 
 server.listen(8000, () => {
