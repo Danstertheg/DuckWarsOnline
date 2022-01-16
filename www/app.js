cca97fb42f71dea64c3c4d95aba5c1b54a2272b2
Danny Silvestre <dsylvester2017@fau.edu>
2022-01-16 00:12:22 -0500

    first working prototype

--- www/app.js
+++ www/app.js
@@ -2,23 +2,31 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const { Server, Socket } = require("socket.io");
 const io = new Server(server);
 // list of players
 var playerList = [];
+var projectiles = [];
 var playerCount = 0;
-const roomSize = 3;
+const roomSize = 4;
 var colors = ["blue","red","green","orange","black","navy"]
-var skins = ["oats","danky","mr.goose"];
+var skins = ["oats","danky","mr.goose","mr.goose"];
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   console.log("__dirname is : "+__dirname)
   res.sendFile(__dirname + '/index.html');
 });
-
+function findPlayerInList(id){
+  for (x in playerList){
+    if (playerList[x].id == String(id) ){
+      return Number(x);
+    }
+  }
+  return null
+ }
 io.on('connection', (socket) => {
-    socket.on('chatmessage', (msg) => {
-      io.emit('chatmessage', msg);
+    socket.on('message', (msg) => {
+      io.emit('message', msg);
     });
 
     socket.on('playerJoined', (playerInitObj) => {
@@ -36,7 +44,8 @@ io.on('connection', (socket) => {
             y:playerPositionY,
             color:playerColor,
             name:playerInitObj["playerName"],
-            skin:playerSkin
+            skin:playerSkin,
+            direction:'l'
           };
           // [{},{},{}]
           playerList.push(newplayer);
@@ -52,11 +61,28 @@ io.on('connection', (socket) => {
       });
       socket.on('playerMovement',(playerMov) => {
         socket.broadcast.emit('playerMovement', playerMov);
+        let id = playerMov['id'];
+        let x = playerMov['x'];
+        let y = playerMov['y'];
+        let direction = playerMov['direction'];
+        playerList[findPlayerInList(id)].x = x;
+        playerList[findPlayerInList(id)].y = y;
+        playerList[findPlayerInList(id)].direction = direction;
+        
       })
-
+      socket.on('playerShot',(projectile) => {
+        console.log(projectile['id'] + " has shot.")
+        projectiles.push(projectile)
+        console.log(projectiles)
+        io.emit('updateProjectiles', projectile);
+      })
+      socket.on('requestProjectileLocation',(projectile)=>{
+        io.emit('updatedProjectiles', projectiles)
+      })
       socket.on('startGame',(lobby) => {
         io.emit('startGame');
       })
+      
 
       socket.on('disconnect', function() {
         console.log("disconnect")
@@ -71,7 +97,52 @@ io.on('connection', (socket) => {
         io.emit('updatePlayers', playerList)
       })
   });
+function calculateProjectilesPath(){
+  let speed = 20;
+  let radius = 10;
+  let playerRadius = 30;
+  let canvasWidth = 1000;
+  for (i in projectiles){
+    if (projectiles[i].direction == 'l')
+    {
+      projectiles[i].x -= speed;
+    }
+    else {
+      projectiles[i].x += speed;
+    }
+  }
+    //console.log(projectiles)
+     for (j in playerList){
+      for (i in projectiles){
+      const dx = projectiles[i].x - playerList[j].x;
+      const dy = projectiles[i].y - playerList[j].y;
+      let distance = Math.sqrt(dx*dx + dy*dy); //pythegorean theorem!
+   // checking for collision between current projectile and its target:
+                if (distance < radius + playerRadius) {
+                if (projectiles[i].damageCounted == false && playerList[j].id != projectiles[i].id) {
+                    // play hit sounds
+                    // deal damage
+                    console.log(playerList[j].name + " was hit.")
+                    io.emit("playerHit", playerList[j].id)
+                    //projectiles[i].target.health -= projectiles[i].damage;
+                    projectiles[i].damageCounted = true;
+                    projectiles.splice(i, 1);
+                    
+                }
+                
+            }
+            else if (projectiles[i].x < 0 - radius * 2 || projectiles[i].x > canvasWidth + radius * 2) {
+              projectiles.splice(i, 1);
+            }
+    }
+  }
+}
 
+function runGame(){
+calculateProjectilesPath()
+}
+
+setInterval(runGame,20)
 server.listen(8000,() => {
   console.log('listening on *:8000');
 });
